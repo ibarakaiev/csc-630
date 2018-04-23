3b0d82fa4bcd5101a9fe88c7faef6108f91d8d7c
ibarakaiev <ibarakaiev@gmail.com>
2018-04-23 12:56:49 -0500

    Homework One

---
+++ Assignments/HWOne/HWOne.h
@@ -0,0 +1,40 @@
+struct node {
+	int data;
+	struct node * next;
+};
+
+
+// precondition:  headP points to a linked list pointer
+// postcondition:  the list has a new element added to the top
+void addToTop(struct node ** headP, int data);
+
+// precondition:  headP points to a non-empty linked list pointer
+// postcondition:  the top element in the list has been removed 
+// return: the data value of the removed element
+int removeFromTop(struct node ** headP);
+
+// precondition:  head is a linked list
+// postcondition:  the contents of the list has been printed
+void printList(struct node * head);
+
+// precondition:  head is a linked list
+// return: the number of nodes on the list
+int length(struct node * head);
+
+// precondtion:  head is a non-empty linked list
+// return: the sum of all data values on the list
+int sum(struct node * head);
+
+// precondition: head is a linked list and 0 <= index < length(head)
+// return: the data value at node n
+int getNth(struct node * head, int index);
+
+// precondition: headP points to a linked list pointer, 0 <= index <= length(head)
+void insertNth(struct node ** headP, int data, int index);
+
+// precondtion: headP points to a linked list pointer
+// postcondition: all elements of the list have been removed
+void removeAll(struct node ** headP);
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# csc-630
+# CSC630 - Algorithms and Data Structures in C
+
+CSC630 is an Honors Computer Science Seminar taught at Phillips Academy. In spring of 2018, CSC630 was taught by Mr. Russell and focused on algorithms and data structures. 
+
+### Description
+
+This repository contains the implementations of various algorithms and data structures such as linked lists, binary trees, etc. It is intended to serve as a reference and should not be used to plagiarize assignments whether you study at Phillips Academy or at any other school.
+
+### Contact
+
+ibarakaiev@gmail.com
